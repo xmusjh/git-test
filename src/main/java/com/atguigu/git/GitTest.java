f7fc48ff5db75bfa115fb5d4a3628abc4db64aa5
sjh <1203911834@qq.com>
2022-11-13 21:45:51 +0800

    local

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("update");
         System.out.println("local update");
         System.out.println("local update");
+        System.out.println("local");
     }
 }
