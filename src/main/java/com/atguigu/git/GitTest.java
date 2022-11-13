f903b4fe31d6ff4ba288845aedc72a5998b2dccd
sjh <1203911834@qq.com>
2022-11-13 22:02:02 +0800

    1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,20 +2,6 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
-        System.out.println("Hello Git2!");
-        System.out.println("Hello Git3!");
-        System.out.println("Hello Git4!");
-        System.out.println("master test!");
-        System.out.println("hot-fix test");
-        System.out.println("push test");
-        System.out.println("pull test");
-        System.out.println("hhhhh");
-        System.out.println("update");
-        System.out.println("local update");
-        System.out.println("local update");
-        System.out.println("local");
-        System.out.println("local  hhh");
         System.out.println("1");
     }
 }
