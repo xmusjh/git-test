572519a46bbb467bd596b26b38c39dfb920ec126
sjh <1203911834@qq.com>
2022-11-13 22:35:19 +0800

    Revert "remote 2"
    This reverts commit 0977c1f6

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,7 +3,5 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
     }
 }
